7ef5f5cc22703359f81c39255a769d6e50ee347c
Nguyen Phuc Son Tung <tungnpsgch16064@fpt.edu.vn>
2021-04-07 18:52:46 +0700

    color

---
+++ lib/ui/color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+//------------------------------------------------------------------------------
+class AppColors {
+  static const Color buttonColor  = Color(0xfff33418);
+  static const Color grey         = Color(0xfff4f0ec);
+  static const Color hintColor    = Color(0xff7f8080);
+}
